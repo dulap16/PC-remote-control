460c24d93df38e834b23f79250a5bdd376e0b167
dulap16 <tudorbrk8@yahoo.com>
2023-05-30 16:16:11 +0300

    Template IR receiver code

--- remote/remote.ino
+++ remote/remote.ino
@@ -1,11 +1,20 @@
 // REMOTE CONTROLLER - ARDUINO HALF
 
-void setup() {
-  // put your setup code here, to run once:
+#include <IRremote.h>
 
+const int RECV_PIN = 7;
+IRrecv irrecv(RECV_PIN);
+decode_results results;
+
+void setup(){
+  Serial.begin(9600);
+  irrecv.enableIRIn();
+  irrecv.blink13(true);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-
+void loop(){
+  if (irrecv.decode(&results)){
+        Serial.println(results.value, HEX);
+        irrecv.resume();
+  }
 }
