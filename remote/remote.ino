35311f6d766c250f8cc76dde6eae60aa1e8e4f3c
dulap16 <tudorbrk8@yahoo.com>
2023-05-30 23:12:08 +0300

    Change remote.ino, first version did not function

--- remote/remote.ino
+++ remote/remote.ino
@@ -2,19 +2,16 @@
 
 #include <IRremote.h>
 
-const int RECV_PIN = 7;
-IRrecv irrecv(RECV_PIN);
-decode_results results;
+#define IR_RECEIVE_PIN 7
 
 void setup(){
   Serial.begin(9600);
-  irrecv.enableIRIn();
-  irrecv.blink13(true);
+  IrReceiver.begin(IR_RECEIVE_PIN);
 }
 
 void loop(){
-  if (irrecv.decode(&results)){
-        Serial.println(results.value, HEX);
-        irrecv.resume();
+  if (IrReceiver.decode()){
+      IrReceiver.resume();
+      Serial.println(IrReceiver.decodedIRData.command);
   }
 }
