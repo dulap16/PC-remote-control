c5f0f8937398d7eb9f0f72ea5c8e16f11e2edbf3
dulap16 <tudorbrk8@yahoo.com>
2023-05-30 23:30:28 +0300

    Add codification

--- remote/remote.ino
+++ remote/remote.ino
@@ -34,6 +34,92 @@ void setup(){
 void loop(){
   if (IrReceiver.decode()){
       IrReceiver.resume();
-      Serial.println(IrReceiver.decodedIRData.command);
+      int code = IrReceiver.decodedIRData.command;
+      switch(code) {
+        case 69: {
+          Serial.println("CH-");
+          break;
+        }
+        case 70: {
+          Serial.println("CH");
+          break;
+        }
+        case 71: {
+          Serial.println("CH+");
+          break;
+        }
+        case 68: {
+          Serial.println("PREV");
+          break;
+        }
+        case 64: {
+          Serial.println("NEXT");
+          break;
+        }
+        case 67: {
+          Serial.println("PLAY/PAUSE");
+          break;
+        }
+        case 7: {
+          Serial.println("VOL-");
+          break;
+        }
+        case 21: {
+          Serial.println("VOL+");
+          break;
+        }
+        case 9: {
+          Serial.println("EQ");
+          break;
+        }
+        case 22: {
+          Serial.println("0");
+          break;
+        }
+        case 25: {
+          Serial.println("100+");
+          break;
+        }
+        case 13: {
+          Serial.println("200+");
+          break;
+        }
+        case 12: {
+          Serial.println("1");
+          break;
+        }
+        case 24: {
+          Serial.println("2");
+          break;
+        }
+        case 94: {
+          Serial.println("3");
+          break;
+        }
+        case 8: {
+          Serial.println("4");
+          break;
+        }
+        case 28: {
+          Serial.println("5");
+          break;
+        }
+        case 90: {
+          Serial.println("6");
+          break;
+        }
+        case 66: {
+          Serial.println(7);
+          break;
+        }
+        case 82: {
+          Serial.println(8);
+          break;
+        }
+        case 74: {
+          Serial.println(9);
+          break;
+        }
+      }
   }
 }
