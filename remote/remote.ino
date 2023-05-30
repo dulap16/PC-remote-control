eeb893e5c6f824c60b9e3e948560ecc6be770513
dulap16 <tudorbrk8@yahoo.com>
2023-05-30 23:17:13 +0300

    Add button - code schematic

--- remote/remote.ino
+++ remote/remote.ino
@@ -1,5 +1,27 @@
 // REMOTE CONTROLLER - ARDUINO HALF
 
+// CH-        69 
+// CH         70 
+// CH+        71
+// PREV       68 
+// NEXT       64 
+// PLAY/PAUSE 67
+// VOL-       7 
+// VOL+       21 
+// EQ         9
+// 0          22 
+// 100+       25 
+// 200+       13
+// 1          12 
+// 2          24 
+// 3          94
+// 4          8 
+// 5          28 
+// 6          90
+// 7          66 
+// 8          82 
+// 9          74
+
 #include <IRremote.h>
 
 #define IR_RECEIVE_PIN 7
